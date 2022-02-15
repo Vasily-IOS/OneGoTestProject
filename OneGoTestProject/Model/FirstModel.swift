2a0d168d9fc1aaccbb9fbc9164bcad98a8bcdf88
vasiliy <vasyaburton91@gmail.com>
2022-02-15 20:17:22 +0300

    Finish

---
+++ OneGoTestProject/Model/FirstModel.swift
@@ -0,0 +1,22 @@
+//
+//  FirstSectionModel.swift
+//  OneGoTestProject
+//
+//  Created by Василий on 15.02.2022.
+//
+
+import Foundation
+
+struct FirstModel {
+
+    let description: String
+
+    static var items = [
+        FirstModel(description: "Я начинающий IOS-разработчик"),
+        FirstModel(description: "Я начинающий IOS-разработчик"),
+        FirstModel(description: "Я начинающий IOS-разработчик"),
+        FirstModel(description: "Я начинающий IOS-разработчик"),
+        FirstModel(description: "Я начинающий IOS-разработчик")
+    ]
+    
+}
